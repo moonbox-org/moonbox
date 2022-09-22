e363725c6d35437493889fa78af9a838cccced91
Giovanni Piccolo <piccolog93@gmail.com>
2022-09-21 20:52:27 -0400

    first draft

---
+++ backend/moonbox-api/src/main/java/org/moonbox/moonboxapi/MoonboxApiApplication.java
@@ -0,0 +1,13 @@
+package org.moonbox.moonboxapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoonboxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MoonboxApiApplication.class, args);
+    }
+
+}
---
+++ backend/moonbox-api/src/test/java/org/moonbox/moonboxapi/MoonboxApiApplicationTest.java
@@ -0,0 +1,15 @@
+package org.moonbox.moonboxapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class MoonboxApiApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
