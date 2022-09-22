f1d5f3d1b867cb1a14cab0b003d964b3c967549a
Giovanni Piccolo <piccolog93@gmail.com>
2022-09-21 20:58:05 -0400

    remove useless imports

--- backend/moonbox-api/src/test/java/org/moonbox/moonboxapi/MoonboxApiApplicationTest.java
+++ backend/moonbox-api/src/test/java/org/moonbox/moonboxapi/MoonboxApiApplicationTest.java
@@ -1,8 +1,6 @@
 package org.moonbox.moonboxapi;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
