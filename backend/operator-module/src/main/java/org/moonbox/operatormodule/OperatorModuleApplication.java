bcf886a66c4d17bd2c73a78ffbbe7400eae50c41
Giovanni Piccolo <piccolog93@gmail.com>
2022-09-21 21:02:55 -0400

    add operator module

---
+++ backend/operator-module/src/main/java/org/moonbox/operatormodule/OperatorModuleApplication.java
@@ -0,0 +1,13 @@
+package org.moonbox.operatormodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OperatorModuleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OperatorModuleApplication.class, args);
+    }
+
+}
---
+++ backend/operator-module/src/test/java/org/moonbox/operatormodule/OperatorModuleApplicationTest.java
@@ -0,0 +1,13 @@
+package org.moonbox.operatormodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class OperatorModuleApplicationTest {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
