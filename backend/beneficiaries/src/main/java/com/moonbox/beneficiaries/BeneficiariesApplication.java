775bca5819f5977a70e48fa9db844031b611054d
Giovanni Piccolo <piccolog93@gmail.com>
2023-02-09 18:28:47 +0100

    add restTemplate and auth header interceptor

--- backend/beneficiaries/src/main/java/com/moonbox/beneficiaries/BeneficiariesApplication.java
+++ backend/beneficiaries/src/main/java/com/moonbox/beneficiaries/BeneficiariesApplication.java
@@ -1,13 +1,24 @@
 package com.moonbox.beneficiaries;
 
+import com.moonbox.beneficiaries.config.AuthorizationHeaderInterceptor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.Collections;
 
 @SpringBootApplication
 public class BeneficiariesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(BeneficiariesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(BeneficiariesApplication.class, args);
+    }
 
+    @Bean
+    public RestTemplate restTemplate() {
+        RestTemplate template = new RestTemplate();
+        template.setInterceptors(Collections.singletonList(new AuthorizationHeaderInterceptor()));
+        return template;
+    }
 }
